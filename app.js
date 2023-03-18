8d19dd0dbf56d941633f0a4a50522a15fdcfa368
mohamedEbrahem <mohamedebrahemprogrammer@gmail.com>
2023-03-18 23:20:17 +0200

    creating html , css and js files

---
+++ app.js
@@ -0,0 +1,4 @@
+const featuresElement = document.querySelector('nav ul li');
+const featuresList = document.querySelector('.features-list');
+console.log(featuresElement);
+console.log(featuresList);
